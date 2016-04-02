66b17ebfd3dafce5fff6dec84057f77cc5708949
mckKatie <kmckinney16@my.whitworth.edu>
2016-04-02 14:33:27 -0700

    modified:   Sim/Sim/DataFile.cs 	modified:   Sim/Sim/Program.cs 	modified:   Sim/Sim/Temp.cs

--- Sim/Sim/Program.cs
+++ Sim/Sim/Program.cs
@@ -11,12 +11,21 @@ namespace Sim
 
         static void Main(string[] args)
         {
-            DataFile dataInfo = new DataFile();
-            dataInfo.getInfoFromFile();
-            FCFS algo1 = new FCFS(1);
-            algo1.getInfo(dataInfo.getDictionary(), dataInfo.getSubTimes());
-            algo1.RunSimulation();
-            Analysis.DisplayAverages(dataInfo.getDictionary());
+            List<Run> runs = new List<Run>();
+            int dataFiles = 1;
+            while (dataFiles != 0)
+            {
+                Run newRun = new Run("FCFS", dataFiles);
+                DataFile dataInfo = new DataFile();
+                dataInfo.getInfoFromFile();
+                FCFS algo1 = new FCFS(1);
+                algo1.getInfo(dataInfo.getDictionary(), dataInfo.getSubTimes());
+                algo1.RunSimulation();
+                newRun.outputInfo(ref dataInfo);
+                runs.Add(newRun);
+                //Analysis.DisplayAverages(dataInfo.getDictionary());
+                dataFiles--;
+            }
         }
     }
 }
